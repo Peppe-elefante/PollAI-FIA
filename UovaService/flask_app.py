bfdac4629f73f1e3e60082756b5ae67b139cde7c
Giuseppe Elefante <giuseppeelefante123@gmail.com>
2025-01-18 15:25:51 +0100

    risoluzione bug

--- UovaService/flask_app.py
+++ UovaService/flask_app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import py_eureka_client.eureka_client as eureka_client
+import socket
+
+# Get the correct IP address to register with Eureka
+hostname = socket.gethostbyname(socket.gethostname())
 
 rest_port = 8050
 eureka_client.init(eureka_server="http://localhost:8761/eureka",
                    app_name="uova-service",
-                   instance_port=rest_port)
+                   instance_port=rest_port,
+                   instance_ip = hostname)
 
 app = Flask(__name__)
 
